e7b56478764f76f2bcf7f628a2c5bb348823f0d8
SilverTosh <silvestermigwi76@gmail.com>
2025-04-13 11:34:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   ownerNumber: "254720326316",
   ownerName: "KINGVON",
-  supportChannel: "https://t.me/KINGVON_SUPPORT",
+  supportChannel: "!https://whatsapp.com/channel/0029Vb5tbcZEKyZEHbicrV1y",
   botName: "KINGVON-XMD",
   prefixes: ['.', '#', '$', '&', '-', '+', '!', '*', '¶'],
   menuType: 'all commands',
-  thumbnailUrl: 'https://path-to-tech-thumbnail-image'
+  thumbnailUrl: 'https://files.catbox.moe/4v6o48.jpg'
 };
